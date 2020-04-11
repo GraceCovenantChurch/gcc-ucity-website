a58e764e843cdadb4b8600cce3fd591386213e27
Handrew <andrew.a.han@gmail.com>
2020-04-11 18:18:54 -0400

    comment out pages not created

--- src/pages/BaseRouter.jsx
+++ src/pages/BaseRouter.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./home/Home";
-import Beliefs from "./beliefs/Beliefs";
-import Page from "./page/Page";
-import AsyncPage from "./asyncPage/AsyncPage";
-import FamilyGroup from "./familyGroup/FamilyGroup";
-import Ministries from "./ministries/Ministries";
-import Staff from "./staff/Staff";
-import Welcome from "./welcome/Welcome";
-import Events from "./events/Events";
-import Multimedia from "./multimedia/Multimedia";
-import Giving from "./giving/Giving";
-import Sermons from "./sermons/Sermons";
+// import Beliefs from "./beliefs/Beliefs";
+// import Page from "./page/Page";
+// import AsyncPage from "./asyncPage/AsyncPage";
+// import FamilyGroup from "./familyGroup/FamilyGroup";
+// import Ministries from "./ministries/Ministries";
+// import Staff from "./staff/Staff";
+// import Welcome from "./welcome/Welcome";
+// import Events from "./events/Events";
+// import Multimedia from "./multimedia/Multimedia";
+// import Giving from "./giving/Giving";
+// import Sermons from "./sermons/Sermons";
 
 const BaseRouter = () => (
   <Router>
     <Route exact path="/" component={Home} />
-    <Route path="/beliefs" component={Beliefs} />
+    {/* <Route path="/beliefs" component={Beliefs} />
     <Route path="/page" component={Page} />
     <Route path="/asyncPage" component={AsyncPage} />
     <Route path="/familyGroup" component={FamilyGroup} />
@@ -27,7 +27,7 @@ const BaseRouter = () => (
     <Route path="/events" component={Events} />
     <Route path="/multimedia" component={Multimedia} />
     <Route path="/giving" component={Giving} />
-    <Route path="/sermons" component={Sermons} />
+    <Route path="/sermons" component={Sermons} /> */}
   </Router>
 );
 
