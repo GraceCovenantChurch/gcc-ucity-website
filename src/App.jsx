d59bde0571472010b4d2d3628d0de3f400eacd40
Handrew <andrew.a.han@gmail.com>
2020-04-11 21:19:40 -0400

    install/setup husky and test it

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./App.module.scss";
 
 import BaseRouter from "./pages/BaseRouter";
+import Footer from "components/footer/Footer";
+// import { doc } from "prettier";
 
 function App() {
   return (
     <div className={styles.App}>
       <div className={styles.AppContent}>
         <BaseRouter />
+          <Footer />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import "./styles/global.scss";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
-    rootElement
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
