2c74ee1e2e7353e2df394a3c9d14129492c5c3a1
Joey Zia <joey.zia@gmail.com>
2021-08-31 15:41:41 -0400

    App Links Updated

--- src/constants/mobileapp.jsx
+++ src/constants/mobileapp.jsx
@@ -1,3 +1,4 @@
-export const APPLE = "https://itunes.apple.com/us/app/id651172729?mt=8&uo=4";
+export const APPLE =
+  "https://apps.apple.com/us/app/gcc-philadelphia/id1446852662";
 export const ANDROID =
-  "https://play.google.com/store/apps/details?id=com.subsplash.thechurchapp.ami&referrer=utm_source%3Dsubsplash%26utm_content%3DeyJoYW5kbGVyIjoiYXBwIiwiYXBwa2V5IjoiNlZaSFZOIn0=";
+  "https://play.google.com/store/apps/details?id=com.subsplashconsulting.s_QZRM2D";
